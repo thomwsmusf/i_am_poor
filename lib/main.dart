bc3bacded3c21c157866d705dd152e01c9a53a12
Thomas W S Moura <thomas.moura@mail.usf.edu.br>
2019-08-26 23:19:10 -0300

    Add app icon and background Add main screen layout Add assets: images/

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,24 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Text('I' 'm poor!'),
+      home: Scaffold(
+        backgroundColor: Colors.deepPurpleAccent[200],
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurpleAccent[700],
+          title: Center(
+            child: Text(
+              'I am poor!',
+              style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 28),
+            ),
+          ),
+        ),
+        body: Center(
+          child: Image(image: AssetImage('images/cut_money.png')),
+        ),
+      ),
     ),
   );
 }
